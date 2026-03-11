b14f5ed75b2f75ab9e502e222936ac9ed9940ff6
noumanshah436 <noumanshah436@gmail.com>
2026-03-11 19:45:37 +0500

    Learning

--- Next.js-15-Masterclass/17. Backend/7. Middlewares/2. Conditional Redirects/middleware.ts
+++ Next.js-15-Masterclass/17. Backend/7. Middlewares/2. Conditional Redirects/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/dashboard") {
     return NextResponse.redirect(new URL("/register", request.nextUrl));
+    // return NextResponse.json({ success: "Successfully ran"});
   }
 }
 
